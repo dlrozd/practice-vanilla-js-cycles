8cdb705e5ea8647cabb08cfee0851742013809ba
drozd <mayakovsky03@gmail.com>
2025-12-14 14:19:07 +0200

    refactoring code to find best cost and score and update logic to use it

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// data
+
 const scores = [
     54, 22, 32, 11, 33,
     44, 22, 26, 74, 92,
     90, 84, 43, 67, 92,
     90, 53, 33, 11, 23
+]
+
+const costs = [
+    .54, .22, .32, .11, .33,
+    .44, .22, .26, .74, .92,
+    .90, .84, .43, .67, .92,
+    .90, .53, .33, .11, .23
 ]
 
 // cycle while
@@ -20,19 +29,23 @@ const scores = [
 
 // cycle for
 
+// logic
 
 const bestScore = printAndGetHighScore(scores);
 const bestScores = getBestScores(scores, bestScore);
+const bestIndex = getBestCostAndScore(scores, costs, bestScore);
 
-function getBestScores(array, value) {
+
+function getBestScores(array, maxValue) {
     let bestScores = [];
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === value) {
+        if (array[i] === maxValue) {
             bestScores.push(i);
         }
     }
     return bestScores;
 }
+
 function printAndGetHighScore(array) {
     let result;
     let highScore = 0;
@@ -47,6 +60,24 @@ function printAndGetHighScore(array) {
     return highScore;
 }
 
-console.log('All scores: ' + scores.length)
-console.log('Highest score from data: ' + bestScore)
-console.log('Best scores of indexes' + ' ' + bestScores + ' ' + 'are' + ' ' + 'number' + ' ' + bestScore)
+function getBestCostAndScore(resultArray, costsArray, maxValue) {
+    let index;
+    let cost = 100;
+
+    for (let i = 0; i < resultArray.length; i++) {
+        if (resultArray[i] === maxValue) {
+            if (costsArray[i] < cost) {
+                cost = costsArray[i];
+                index = i;
+            }
+        }
+    }
+    return index
+}
+
+
+console.log('All scores are: ' + scores.length)
+console.log('Highest score from data is: ' + bestScore)
+console.log('Best scores of indexes are' + ' ' + bestScores + ' ' + 'and' + ' ' + 'it is the number' + ' ' + bestScore)
+console.log('Best index is: ' + bestIndex);
+
